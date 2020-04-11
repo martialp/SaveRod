232682adfc4f82915947984eeafd923b5e47c9a4
Martial Pageau <martial.pageau@gmail.com>
2020-04-11 19:20:10 -0400

    Register OnEntityDeathEvent diff --git a/SaveRod/src/me/qintinator/saverod/Main.java b/SaveRod/src/me/qintinator/saverod/Main.java index d6bedc1..15b75ea 100644 --- a/SaveRod/src/me/qintinator/saverod/Main.java +++ b/SaveRod/src/me/qintinator/saverod/Main.java @@ -1,5 +1,6 @@  package me.qintinator.saverod;
    +import me.qintinator.saverod.eventlisteners.OnEntityDeathEvent;
     import org.bukkit.Bukkit;
     import org.bukkit.plugin.java.JavaPlugin;
    
    @@ -9,25 +10,26 @@ import me.qintinator.saverod.eventlisteners.OnPlayerDeath;
     import me.qintinator.saverod.statics.Bootstrapper;
     import me.qintinator.saverod.statics.ConfigPropertyMapper;
    
    -public class Main extends JavaPlugin{
    -
    -
    -	@Override
    -	public void onEnable() {
    -
    -		saveDefaultConfig();
    -
    -		// running the bootstrapper
    -		Bootstrapper.run();
    -
    -		// running the property mapper
    -		ConfigPropertyMapper.run(this);
    -
    -		// loading the commands
    -		Bukkit.getPluginCommand("rod").setExecutor(new RodCommand(Bootstrapper.getSaverodService()));
    -		Bukkit.getPluginCommand("saverod").setExecutor(new SaveRodCommand(this));
    -
    -		// loading all events
    -		Bukkit.getPluginManager().registerEvents(new OnPlayerDeath(Bootstrapper.getSaverodService(), Bootstrapper.getConfigPropertyService()), this);
    -	}
    +public class Main extends JavaPlugin {
    +
    +
    +    @Override
    +    public void onEnable() {
    +
    +        saveDefaultConfig();
    +
    +        // running the bootstrapper
    +        Bootstrapper.run();
    +
    +        // running the property mapper
    +        ConfigPropertyMapper.run(this);
    +
    +        // loading the commands
    +        Bukkit.getPluginCommand("rod").setExecutor(new RodCommand(Bootstrapper.getSaverodService()));
    +        Bukkit.getPluginCommand("saverod").setExecutor(new SaveRodCommand(this));
    +
    +        // loading all events
    +        Bukkit.getPluginManager().registerEvents(new OnPlayerDeath(Bootstrapper.getSaverodService(), Bootstrapper.getConfigPropertyService()), this);
    +        Bukkit.getPluginManager().registerEvents(new OnEntityDeathEvent(Bootstrapper.getSaverodService()), this);
    +    }
     }

--- SaveRod/src/me/qintinator/saverod/Main.java
+++ SaveRod/src/me/qintinator/saverod/Main.java
@@ -1,5 +1,6 @@
 package me.qintinator.saverod;
 
+import me.qintinator.saverod.eventlisteners.OnEntityDeathEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,25 +10,26 @@ import me.qintinator.saverod.eventlisteners.OnPlayerDeath;
 import me.qintinator.saverod.statics.Bootstrapper;
 import me.qintinator.saverod.statics.ConfigPropertyMapper;
 
-public class Main extends JavaPlugin{
-	
-	
-	@Override
-	public void onEnable() {
-			
-		saveDefaultConfig();
-		
-		// running the bootstrapper
-		Bootstrapper.run();
-				
-		// running the property mapper
-		ConfigPropertyMapper.run(this);
-		
-		// loading the commands
-		Bukkit.getPluginCommand("rod").setExecutor(new RodCommand(Bootstrapper.getSaverodService()));
-		Bukkit.getPluginCommand("saverod").setExecutor(new SaveRodCommand(this));
-		
-		// loading all events
-		Bukkit.getPluginManager().registerEvents(new OnPlayerDeath(Bootstrapper.getSaverodService(), Bootstrapper.getConfigPropertyService()), this);
-	}	
+public class Main extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+
+        saveDefaultConfig();
+
+        // running the bootstrapper
+        Bootstrapper.run();
+
+        // running the property mapper
+        ConfigPropertyMapper.run(this);
+
+        // loading the commands
+        Bukkit.getPluginCommand("rod").setExecutor(new RodCommand(Bootstrapper.getSaverodService()));
+        Bukkit.getPluginCommand("saverod").setExecutor(new SaveRodCommand(this));
+
+        // loading all events
+        Bukkit.getPluginManager().registerEvents(new OnPlayerDeath(Bootstrapper.getSaverodService(), Bootstrapper.getConfigPropertyService()), this);
+        Bukkit.getPluginManager().registerEvents(new OnEntityDeathEvent(Bootstrapper.getSaverodService()), this);
+    }
 }
